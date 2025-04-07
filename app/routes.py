39a9519c56e784c482eeca1ab7bf0ed96c5145d5
abangzhu <348188247@qq.com>
2025-04-07 23:58:41 +0800

    c6

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 from app import app
 from app import db
 from flask import render_template, flash, redirect,url_for
-from app.forms import LoginForm
+from app.forms import LoginForm,RegistrationForm,EditProfileForm
 from flask_login import current_user, login_user,logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
 from urllib.parse import urlsplit
 from flask import request
-from app.forms import RegistrationForm
+from datetime import datetime,timezone
 
 @app.route('/')
 @app.route('/index')
@@ -54,8 +54,8 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        form.validate_username(form.username.data)
-        form.validate_email(form.email.data)
+        # form.validate_username(form.username.data)
+        # form.validate_email(form.email.data)
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
@@ -64,3 +64,35 @@ def register():
         return redirect(url_for('login'))
     # 确保在表单验证失败时返回响应
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author':user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    # 确保在GET请求和表单验证失败时返回响应
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
